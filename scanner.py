3acb68373a114bb00ca4de083279248047c1bb59
kokhant9 <kyaw@example.com>
2026-02-16 20:26:46 +0000

    Upgrade: Added Banner Grabbing feature

--- scanner.py
+++ scanner.py
@@ -1,20 +1,25 @@
 import socket
-import threading
 
 target = "8.8.8.8"
+ports = [21, 22, 80, 443]
 
-def port_scan(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1)
-    result = s.connect_ex((target, port))
-    if result == 0:
-        print(f"Port {port}: OPEN ✅")
-    s.close()
+print(f"Scanning & Grabbing Banners on {target}...")
 
-print(f"Starting Multi-threaded Scan on {target}...")
+for port in ports:
+    try:
+        s = socket.socket()
+        s.settimeout(2)
+        s.connect((target, port))
+        
+        # Banner ကို လှမ်းတောင်းကြည့်မယ်
+        s.send(b'Hello\r\n')
+        banner = s.recv(1024).decode().strip()
+        
+        print(f"Port {port}: OPEN ✅ | Banner: {banner}")
+    except:
+        print(f"Port {port}: OPEN ✅ | Banner: No info (Firewalled?)")
+    finally:
+        s.close()
 
-# Port 1 ကနေ 500 အထိ တစ်ပြိုင်တည်း စစ်မယ်
-for port in range(1, 501):
-    thread = threading.Thread(target=port_scan, args=(port,))
-    thread.start()
+print("Mission Finished!")
 
